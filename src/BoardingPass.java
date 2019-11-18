7fb6f1235d68f16e934fb3848654b0a58ce45d2c
Stephan Zapodeanu <szapodeanu19@gmail.com>
2019-11-18 15:37:16 -0500

    BoardingPass and Passenger Class

--- src/BoardingPass.java
+++ src/BoardingPass.java
@@ -7,4 +7,24 @@
  */
 
 public class BoardingPass {
+    private Passenger passenger;
+    private Gate gate;
+    private Airline airline;
+
+    public BoardingPass(Passenger passenger, Gate gate, Airline airline) {
+        this.passenger = passenger;
+        this.gate = gate;
+        this.airline = airline;
+    }
+
+    public void printInfo() {
+        System.out.println("--------------------------------------------------------------------");
+        System.out.printf("BOARDING PASS for FLIGHT 18000 WITH %s \n", airline.getName());
+        System.out.printf("PASSENGER FIRST NAME: %s \n", passenger.getFirstName());
+        System.out.printf("PASSENGER LAST NAME: %s \n", passenger.getLastName());
+        System.out.printf("PASSENGER AGE: %d \n", passenger.getAge());
+        System.out.printf("You can now begin boarding at gate %s \n", gate.getGate());
+        System.out.println("--------------------------------------------------------------------");
+
+    }
 }
