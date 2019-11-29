694795947491441d323b4d7ab9c02bbd3fc9341a
Stephan Zapodeanu <szapodeanu19@gmail.com>
2019-11-29 14:53:27 -0800

    Server part 1

--- src/ReservationServer.java
+++ src/ReservationServer.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * The server can handle multiple clients simultaneously. It will track and record ticket sales by writing
  * to reservations.txt. A sample reservations file can be found in the Starter Code folder.
@@ -9,6 +16,35 @@
  * @version 11/16/19
  */
 
-public class ReservationServer {
+public class ReservationServer implements Runnable {
+    private static int port = 1111;
+    private Socket socket;
 
+    public ReservationServer(Socket socket) {
+        this.socket = socket;
+    }
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("Server Started. Waiting connection to Port 1111");
+            Socket socket = serverSocket.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        System.out.printf("Connection received from port %d", port);
+        try {
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectOutputStream.flush();
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+            //objectInputStream.close();
+            //objectOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
