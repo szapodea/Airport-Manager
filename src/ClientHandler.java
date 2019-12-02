063cfbf771c618481d09a0144f0da9b92a52e0fe
Stephan Zapodeanu <szapodeanu19@gmail.com>
2019-12-01 19:57:46 -0500

    Server connects, need to connect to GUI

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,7 +1,4 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -17,11 +14,11 @@ import java.net.Socket;
 public class ClientHandler implements Runnable {
 
     private Socket socket;
-    ObjectInputStream socketReader;
-    ObjectOutputStream socketWriter;
-    Delta delta;
-    Southwest southwest;
-    Alaska alaska;
+    private ObjectInputStream socketReader;
+    private ObjectOutputStream socketWriter;
+    private Delta delta;
+    private Southwest southwest;
+    private Alaska alaska;
 
     public ClientHandler(Socket socket, Delta delta, Southwest southwest, Alaska alaska) throws NullPointerException {
         this.socket = socket;
@@ -45,7 +42,7 @@ public class ClientHandler implements Runnable {
         try {
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
             objectOutputStream.flush();
-            PrintWriter printWriter = new PrintWriter(socket.getOutputStream());
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("reservations.txt"));
             ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
 
             socketWriter.writeObject(delta);
@@ -55,6 +52,56 @@ public class ClientHandler implements Runnable {
             Airline airline;
 
             airline = (Airline) socketReader.readObject();
+
+            if (airline instanceof Delta) {
+                delta = (Delta) airline;
+                bufferedWriter.write("Delta");
+                bufferedWriter.newLine();
+                bufferedWriter.write(delta.getPassengers().size() + "/" + delta.getCapacity());
+                bufferedWriter.newLine();
+                bufferedWriter.write("Delta Passenger List");
+                for (Passenger passenger : delta.getPassengers()) {
+                    bufferedWriter.newLine();
+                    bufferedWriter.write(passenger.getLastName().charAt(0) + ". " + passenger.getFirstName() +
+                            ", " + passenger.getAge());
+                    bufferedWriter.newLine();
+                    bufferedWriter.write("-------------------DELTA");
+                }
+                bufferedWriter.newLine();
+            } else if (airline instanceof Alaska) {
+                alaska = (Alaska) airline;
+                bufferedWriter.write("Alaska");
+                bufferedWriter.newLine();
+                bufferedWriter.write(alaska.getPassengers().size() + "/" + alaska.getCapacity());
+                bufferedWriter.newLine();
+                bufferedWriter.write("Alaska Passenger List");
+                for (Passenger passenger : alaska.getPassengers()) {
+                    bufferedWriter.newLine();
+                    bufferedWriter.write(passenger.getLastName().charAt(0) + ". " + passenger.getFirstName() +
+                            ", " + passenger.getAge());
+                    bufferedWriter.newLine();
+                    bufferedWriter.write("-------------------Alaska");
+                }
+                bufferedWriter.newLine();
+            } else if (airline instanceof Southwest) {
+                southwest = (Southwest) airline;
+                bufferedWriter.write("Southwest");
+                bufferedWriter.newLine();
+                bufferedWriter.write(southwest.getPassengers().size() + "/" + southwest.getCapacity());
+                bufferedWriter.newLine();
+                bufferedWriter.write("Southwest Passenger List");
+                for (Passenger passenger : southwest.getPassengers()) {
+                    bufferedWriter.newLine();
+                    bufferedWriter.write(passenger.getLastName().charAt(0) + ". " + passenger.getFirstName() +
+                            ", " + passenger.getAge());
+                    bufferedWriter.newLine();
+                    bufferedWriter.write("-------------------Southwest");
+                }
+                bufferedWriter.newLine();
+            }
+            bufferedWriter.write("EOF");
+            bufferedWriter.close();
+
             //objectInputStream.close();
             //objectOutputStream.close();
         } catch (IOException | ClassNotFoundException e) {
